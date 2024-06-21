3b4397e261a22e464ce81610bac3fc4560fee845
Jessica Grindheim <jmgrindheim@gmail.com>
2024-06-21 12:15:26 -0700

    init files for 002

---
+++ scripts/002.R_programming/002.config.R
@@ -0,0 +1,38 @@
+counter.config = 0
+
+# ---- lib ----
+counter.config = counter.config + 1
+message(counter.config,": lib")
+
+packages = c(
+  # generates a path to the Rproj on the local machine
+  "here"
+)
+
+
+# ---- setup output directory and counters ---- 
+counter.config = counter.config + 1
+message(counter.config,": setup output directory and counters")
+
+# output
+step="002"
+stepDIR=here("results",paste0(step,".", "R_programming", "/"))
+if(!dir.exists(stepDIR)){dir.create(stepDIR)}
+message("Local output path: ", stepDIR)
+
+outdataDIR=here("outdata",paste0(step,".", "R_programming", "/"))
+if(!dir.exists(outdataDIR)){dir.create(outdataDIR)}
+# directory for writing tables that describe samples delivered, etc
+tableDIR = paste0(stepDIR, "tables/")
+if(!dir.exists(tableDIR)){dir.create(tableDIR)}
+# counter for numbering figures
+counter.fig = 0
+counter.table = 0
+counter.appendix = 0
+counter.vignette = 0
+
+
+
+# ---- define recurrent parameters ---- 
+counter.config = counter.config + 1
+message(counter.config,": define recurrent parameters")
